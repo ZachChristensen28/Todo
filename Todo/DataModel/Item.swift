fb917f98db4de8d99c4b097179d8a53fe20089b4
ZachChristensen28 <zachchristensen28@gmail.com>
2018-03-04 22:26:29 -0700

    Initial commit

---
+++ Todo/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Sight Syndicate on 3/4/18.
+//  Copyright © 2018 Zach Christensen. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
